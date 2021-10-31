e130bef2690383f9eb1f20f07b208b6fbfb5acdb
AshamolCM <cmashamolcm@gmail.com>
2021-10-31 18:38:36 +0100

    seal and sign

---
+++ cmdJarBuilder/src/com/java/sample/SeealTest.java
@@ -0,0 +1,10 @@
+package com.java.sample;
+
+public class SeealTest{
+
+  public static void main(String[] args){
+    System.out.println("Hello World from cmd seal Jar");
+    World world = new World();
+    world.sayHello();
+  }
+}
